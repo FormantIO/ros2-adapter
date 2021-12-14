eb3231e5749338635b093b4618b9bf2e6554954f
Matthew Turnshek <matthewturnshek@gmail.com>
2021-12-14 15:14:30 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-source /opt/ros/eloquent/setup.bash
+source /opt/ros/eloquent/setup.bash  # this adapter is meant to work with any ROS2 distribution eloquent+
+# if you use custom messages, source your workspace here
 python3 -m pip install -r requirements.txt
 python3 main.py
