f2c125cb971251067c9570d866fca45b4cb71b58
Matthew Turnshek <matthewturnshek@gmail.com>
2022-01-03 15:27:58 -0500

    fix QoS profile compatibility issues, add more error logs

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import importlib
 
 import rclpy
+from rclpy.qos import qos_profile_sensor_data
 from std_msgs.msg import (
     Bool,
     Char,
@@ -39,8 +40,6 @@ from message_utils.utils import (
     message_to_json,
     get_message_path_value,
 )
-
-QOS_PROFILE = 10
 
 
 class Adapter:
@@ -210,6 +209,7 @@ class Adapter:
                             )
                         )
                     except Exception as e:
+                        print("Error ingesting " + stream + ": " + str(e))
                         return
                 elif type(message) == PointCloud2:
                     try:
@@ -223,6 +223,7 @@ class Adapter:
                             )
                         )
                     except Exception as e:
+                        print("Error ingesting " + stream + ": " + str(e))
                         return
                 else:  # Ingest any messages without a direct mapping to a Formant type as JSON
                     self.fclient.post_json(stream, message_to_json(message))
@@ -237,7 +238,9 @@ class Adapter:
             for (
                 topic_name,
                 topic_types,
-            ) in self.node.get_publisher_names_and_types_by_node(node_name, node_namespace):
+            ) in self.node.get_publisher_names_and_types_by_node(
+                node_name, node_namespace
+            ):
                 if topic_name not in self.get_configured_topics():
                     continue
                 if len(topic_types) == 0:
@@ -261,7 +264,7 @@ class Adapter:
                 message_type,
                 topic,
                 lambda m, t=topic: self.message_callback(t, m),
-                QOS_PROFILE,
+                qos_profile_sensor_data,
             )
 
         # Modify any existing subscriptions whose type has changed
@@ -275,7 +278,7 @@ class Adapter:
                     latest_message_type,
                     topic,
                     lambda m, t=topic: self.message_callback(t, m),
-                    QOS_PROFILE,
+                    qos_profile_sensor_data,
                 )
 
 
