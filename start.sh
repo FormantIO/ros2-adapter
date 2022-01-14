9f58f83090b6d0734195394b848ae2243376a55d
Cameron Schloer <cameron.schloer@fireflyautomatix.com>
2022-01-14 15:19:58 -0700

    fix(start.sh): Aligning with main repo master. No longer need unique

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# this adapter is meant to work with any ROS2 distribution eloquent+, soruce before use.
-# if you use custom messages, source your workspace before use.
 
+source /opt/ros/eloquent/setup.bash  # this adapter is meant to work with any ROS2 distribution eloquent+
+# if you use custom messages, source your workspace here
 python3 -m pip install -r requirements.txt
 cd formant_ros2_adapter/scripts/
 python3 main.py
