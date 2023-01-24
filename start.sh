2ffcc559760b50edce9b8a0bbf5b8462ee6cdf66
Aaron Boswell <aaron@formant.io>
2023-01-24 15:08:51 -0500

    remove custom wheel

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 source /opt/ros/*/setup.bash  # this adapter is meant to work with any ROS2 distribution
-python3 -m pip install --force-reinstall --upgrade formant-1.105.50_release_1.105_075d6b4c-py2.py3-none-any.whl
 
 # if you use custom messages, source your workspace here
 python3 -m pip install -r requirements.txt
