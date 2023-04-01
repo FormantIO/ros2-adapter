9040501356a85b26e29bd619281dcbc279eaf692
nbinford-formant <119459659+nbinford-formant@users.noreply.github.com>
2023-04-01 14:35:06 -0700

    Query for subscribers test

--- tests/test_subscribers.sh
+++ tests/test_subscribers.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+
+DEVICE="nicolas-container"
+TOPIC="/nicolas_string"
+STREAM="nicolas.string"
+
 source /opt/ros/*/setup.bash
-ros2 topic pub -t 10 -w 0 /qa_string std_msgs/msg/String "data: {hello: world}"
+
+# How to get date formant required for fctl
+start_time=$(date -u +"%Y-%m-%dT%H:%M:%S")
+ros2 topic pub -t 2 -w 0 $TOPIC std_msgs/msg/String "data: {key: value}"
+end_time=$(date -u +"%Y-%m-%dT%H:%M:%S")
+
+# Verify that the data just published was ingested with fctl
+fctl query --device $DEVICE --stream $STREAM --start $start_time --end $end_time
