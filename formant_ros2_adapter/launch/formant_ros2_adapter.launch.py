731e443f03ba6c517a4255a98e5ba43cea9c5750
Andres Galaviz <5544243+andresgalaviz@users.noreply.github.com>
2023-07-14 01:00:03 +0000

    minor cleanup

--- formant_ros2_adapter/launch/formant_ros2_adapter.launch.py
+++ formant_ros2_adapter/launch/formant_ros2_adapter.launch.py
@@ -5,7 +5,6 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 
-
 def generate_launch_description():
     # Get the path to the package
     package_name = "formant_ros2_adapter"
