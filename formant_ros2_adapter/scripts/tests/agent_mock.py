60fa5624d6812c57fe79c2f7cc06d64d1d96c208
Niranjan Krishna <niranjan@formant.io>
2023-10-29 02:25:10 +0530

    FormantControl up and running; Subscribers the next step

--- formant_ros2_adapter/scripts/tests/agent_mock.py
+++ formant_ros2_adapter/scripts/tests/agent_mock.py
@@ -1,54 +1,98 @@
 import grpc
-from concurrent import futures
 from formant.protos.agent.v1 import agent_pb2, agent_pb2_grpc
-from formant.protos.model.v1 import commands_pb2
-from models_mock import (
-    CommandRequest,
-    ApplicationConfiguration,
-)
+from formant.protos.model.v1 import commands_pb2, datapoint_pb2, math_pb2
+from concurrent import futures
 
 
 class AgentMockServicer(agent_pb2_grpc.AgentServicer):
     def __init__(self):
         super().__init__()
-
         self.post_datapoints = []
 
     def GetAgentConfiguration(self, request, context):
-        return agent_pb2.GetApplicationConfigurationResponse(request)
+        return agent_pb2.GetAgentConfigurationResponse(
+            configuration=agent_pb2.AgentConfiguration(request)
+        )
 
     def GetConfigBlobData(self, request, context):
-        return agent_pb2.GetConfigBlobDataResponse(blob_data={"blob_key": "blob_value"})
+        return agent_pb2.GetConfigBlobDataResponse(
+            blob_data=agent_pb2.BlobData(request)
+        )
 
     def GetApplicationConfiguration(self, request, context):
-        return agent_pb2.GetApplicationConfigurationResponse(configuration=request)
-
-    def GetCommandRequestStream(self, request, context):
-        print(type(request))
-        command_request = commands_pb2.CommandRequest()
-        yield agent_pb2.GetCommandRequestStreamResponse(request=request)
-
-    def SendCommandResponse(self, request, context):
-        return agent_pb2.SendCommandResponseResponse()
+        return agent_pb2.GetApplicationConfigurationResponse(
+            configuration=agent_pb2.ApplicationConfiguration(request)
+        )
 
     def GetTeleopControlDataStream(self, request, context):
-        yield agent_pb2.GetTeleopControlDataStreamResponse()
+        # Check if the client is interested in the "Buttons" stream
+        if "Buttons" in request.stream_filter:
+            while True:
+                yield agent_pb2.GetTeleopControlDataStreamResponse(
+                    control_datapoint=datapoint_pb2.ControlDatapoint(
+                        stream="Buttons",
+                        timestamp=1234567890,  # Mock timestamp
+                        numeric=math_pb2.Numeric(value=42.0),  # Mock timestamp
+                        bitset=datapoint_pb2.Bitset(
+                            bits=[datapoint_pb2.Bit(key="SomeButtonKey", value=True)]
+                        ),
+                    )
+                )  # Mock data for the "Buttons" stream
 
-    def PostDataMulti(self, request, context):
-        print("hey", request.datapoints)
-        self.post_datapoints.append(request.datapoints)
+        else:
+            # Handle other stream filters or return an error
+            pass
+
+    def GetCommandRequestStream(self, request, context):
+
+        # Check if the client is interested in the "Buttons" stream
+        if "Buttons" in request.command_filter:
+            while True:
+                # Generate and yield appropriate responses based on the "Buttons" filter
+                # For example:
+                yield agent_pb2.GetCommandRequestStreamResponse(
+                    request=commands_pb2.CommandRequest(
+                        id="ButtonsId", command="Buttons", text="ButtonsText"
+                    )
+                )
+        else:
+            # Handle other command filters or return an error
+            pass
+
+    def SendCommandResponse(self, request, context):
+        return agent_pb2.SendCommandResponseResponse(request)
+
+    def PostDataMulti(self, request: agent_pb2.PostDataMultiRequest, context):
+        # Add the datapoints from the request to the list
+        self.post_datapoints.extend(request.datapoints)
+        print(request, self.post_datapoints)
+        context.set_code(grpc.StatusCode.OK)
+        context.set_details("Request processed successfully")
+
+        # Return a simple response to the client
         return agent_pb2.PostDataMultiResponse()
 
-    def StreamData(self, request, context):
-        return agent_pb2.StreamDataResponse()
+
+class RequestInterceptor(grpc.ServerInterceptor):
+    def intercept_service(self, continuation, handler_call_details):
+        # Print the handler call details
+        # print("Handler Call Details:", handler_call_details)
+
+        # Continue with the given service handler
+        return continuation(handler_call_details)
 
 
-def serve(servicer: AgentMockServicer):
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    agent_pb2_grpc.add_AgentServicer_to_server(servicer, server)
-
+def serve():
+    interceptor = RequestInterceptor()
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10), interceptors=[interceptor]
+    )
+    agent_pb2_grpc.add_AgentServicer_to_server(AgentMockServicer(), server)
     server.add_insecure_port("[::]:50051")
+    print("Server started at [::]:50051")
     server.start()
-    print("AgentMock Server started on port 50051")
+    server.wait_for_termination()
 
-    return server
+
+if __name__ == "__main__":
+    serve()
--- formant_ros2_adapter/scripts/tests/test_subscribers/test_subscribers.py
+++ formant_ros2_adapter/scripts/tests/test_subscribers/test_subscribers.py
@@ -18,28 +18,30 @@ class TestAgentMockServicer(unittest.TestCase):
 
         terminal_command = "gnome-terminal"
         script_to_run = "source /opt/ros/*/setup.bash && python3 ../main.py --test"
+        self.f = open("output.txt", "w")
         self.adapter_process = subprocess.Popen(
             script_to_run,
             shell=True,
             executable="/bin/bash",
             cwd="./",
+            stdout=self.f,
+            stderr=self.f,
         )
-        time.sleep(30)
+        time.sleep(10)
 
     def test_post_string(self):
         topic = "/my_string"
         message_data = String()
         message_data.data = "key: value"
-
+        """
         start_time = datetime.datetime.utcnow().isoformat()
         print(f"Start Time: {start_time}")
 
-        general_publisher = GeneralPublisher(topic, String, message_data)
+        _ = GeneralPublisher(topic, String, message_data)
 
         end_time = datetime.datetime.utcnow().isoformat()
         print(f"End Time: {end_time}")
-
-        time.sleep(5)
+        """
         print(self.servicer.post_datapoints)
 
     def tearDown(self):
@@ -48,6 +50,7 @@ class TestAgentMockServicer(unittest.TestCase):
         self.adapter_process.wait()  # Wait for the process to terminate
 
         self.server.stop(0)
+        self.f.close()
 
         rclpy.shutdown()
 
