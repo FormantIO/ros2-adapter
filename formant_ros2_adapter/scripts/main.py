e840581bf4d87606fe61196fa5735b16b2f27f70
Niranjan Krishna <niranjan@formant.io>
2024-02-01 02:17:48 +0530

    added minimal main.py

--- formant_ros2_adapter/scripts/main.py
+++ formant_ros2_adapter/scripts/main.py
@@ -1,49 +1,23 @@
 #!/usr/bin/python3
-
-import os
 import rclpy
+from rclpy.node import Node
 
-from formant.sdk.agent.v1 import Client
-from ros2_adapter import ROS2Adapter
-from utils.logger import get_logger
-import os
+class EmptyNode(Node):
+    def __init__(self):
+        super().__init__('empty_node')
+        self.get_logger().info('EmptyNode is running...')
 
-ROS2_DOMAIN_ID = os.environ.get("ROS_DOMAIN_ID", None)
-if ROS2_DOMAIN_ID:
-    ROS2_DOMAIN_ID = int(ROS2_DOMAIN_ID)
+def main(args=None):
+    rclpy.init(args=args)
+    node = EmptyNode()
 
-FCLIENT_WAIT = 2
-
-if __name__ == "__main__":
-    logger = get_logger()
-    rclpy.init(domain_id=ROS2_DOMAIN_ID)
-
-
-    # Get the config directory from an environment variable
-    config_dir = os.environ.get("CONFIG_DIR")
-
-    # If the config environment variable is set and exists, change the working directory
-    # Workaround for the agent adapter looking for "config.py" in the current working directory only
-    if config_dir and os.path.isdir(config_dir):
-        os.chdir(config_dir)
-
-    node = rclpy.create_node(
-        "formant_ros2_adapter",
-        allow_undeclared_parameters=True,
-        automatically_declare_parameters_from_overrides=True,
-    )
-    logger.info("Creating Formant agent client")
-    # To do: a cleaner solution would have ignore_unavailable=True and
-    # something implemented in the client to avoid a race condition
-    fclient = Client(ignore_throttled=True, ignore_unavailable=True)
-    logger.info("Waiting %s seconds for Formant agent client" % FCLIENT_WAIT)
-    ROS2Adapter(fclient, node)
     try:
-        while rclpy.ok():
-            rclpy.spin_once(node, timeout_sec=1.0)
+        rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-    if node:
+    finally:
         node.destroy_node()
+        rclpy.shutdown()
 
-    rclpy.shutdown()
+if __name__ == '__main__':
+    main()
